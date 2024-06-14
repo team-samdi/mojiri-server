cf153bb8ab02e98902351471e54e69c103bf0c56
lgwk42 <luisgunkim@gmail.com>
2024-06-14 19:27:39 +0900

    first commit

---
+++ src/main/java/com/project/mojito_server/MojitoServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.mojito_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojitoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojitoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mojito_server/MojitoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mojito_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojitoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
